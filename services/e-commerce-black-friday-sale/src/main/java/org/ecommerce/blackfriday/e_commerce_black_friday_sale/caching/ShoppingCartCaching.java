2c67d53468f90338135d72002a2dfd033c87d740
hans.arias <hansarias74@gmail.com>
2025-04-02 00:35:36 -0500

    [Backend] add redis to pom.xml

---
+++ services/e-commerce-black-friday-sale/src/main/java/org/ecommerce/blackfriday/e_commerce_black_friday_sale/caching/ShoppingCartCaching.java
@@ -0,0 +1,9 @@
+package org.ecommerce.blackfriday.e_commerce_black_friday_sale.caching;
+
+import java.io.Serializable;
+
+public class ShoppingCartCaching implements Serializable {
+
+    private String customerId;
+
+}
