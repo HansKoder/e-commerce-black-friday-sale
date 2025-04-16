42841416303fba5398c1cab0c31c8095a2a69124
hans.arias <hansarias74@gmail.com>
2025-04-16 17:03:55 -0500

    [Backend] add use case create cart

---
+++ services/e-commerce-black-friday-sale/src/main/java/org/ecommerce/blackfriday/cart/application/service/CreateCartService.java
@@ -0,0 +1,18 @@
+package org.ecommerce.blackfriday.cart.application.service;
+
+import org.ecommerce.blackfriday.cart.domain.model.entity.Cart;
+import org.ecommerce.blackfriday.cart.domain.model.repository.CartRepository;
+
+public class CreateCartService {
+
+    private final CartRepository cartRepository;
+
+    public CreateCartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public Cart handler (String customerId, Cart cart) {
+        this.cartRepository.save(customerId, cart);
+        return cart;
+    }
+}
