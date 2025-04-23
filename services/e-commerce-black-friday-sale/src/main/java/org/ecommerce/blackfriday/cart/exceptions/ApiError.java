42d7e8280e200e07a597600d8322d0eb32abf11e
hans.arias <hansarias74@gmail.com>
2025-04-23 01:46:44 -0500

    [Backend] refactor(cart) remove mvc v1

--- services/e-commerce-black-friday-sale/src/main/java/org/ecommerce/blackfriday/cart/exceptions/ApiError.java
+++
@@ -1,4 +0,0 @@
-package org.ecommerce.blackfriday.cart.exceptions;
-
-public record ApiError(int status, String message) {
-}
--- services/e-commerce-black-friday-sale/src/main/java/org/ecommerce/blackfriday/cart/exceptions/CartNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.ecommerce.blackfriday.cart.exceptions;
-
-public class CartNotFoundException extends RuntimeException {
-
-    public CartNotFoundException(String cartId) {
-        super("Cart with the [ID] " + cartId + " not found.");
-    }
-}
