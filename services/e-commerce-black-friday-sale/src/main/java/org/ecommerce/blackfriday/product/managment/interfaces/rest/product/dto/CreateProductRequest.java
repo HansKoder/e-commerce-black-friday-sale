b1d972133e159804fd542c941382291750740364
hans.arias <hansarias74@gmail.com>
2025-04-25 13:18:24 -0500

    [Backend] refactor(product) dto was modified from class to record

--- services/e-commerce-black-friday-sale/src/main/java/org/ecommerce/blackfriday/product/managment/interfaces/rest/product/dto/CreateProductRequest.java
+++ services/e-commerce-black-friday-sale/src/main/java/org/ecommerce/blackfriday/product/managment/interfaces/rest/product/dto/CreateProductRequest.java
@@ -1,35 +1,16 @@
 package org.ecommerce.blackfriday.product.managment.interfaces.rest.product.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
-public class CreateProductRequest {
-
-    @NotNull
-    @NotEmpty
-    @Length(min = 3)
-    private String productName;
-
-    @NotNull
-    @NotEmpty
-    @Length(min = 10)
-    private String productDescription;
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-}
+public record CreateProductRequest(
+        @NotNull
+        @NotEmpty
+        @Length(min = 3)
+        String productName,
+        @NotNull
+        @NotEmpty
+        @Length(min = 10)
+        String productDescription
+) { }
